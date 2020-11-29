74979863f5a24c3a59e72a3d74082b306a388b8c
Kevin Cao <kcao1998@gmail.com>
2020-11-29 01:30:37 -0500

    able to fetch most recent chapter number

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import requests
+import re
+from bs4 import BeautifulSoup
 
 def save_html(html, path):
     """Writes an HTML file to persistent storage."""
@@ -10,9 +12,20 @@ def open_html(path):
     with open(path, 'rb') as f:
         return f.read()
 
-url = 'https://manganelo.com/chapter/karakai_jouzu_no_takagisan/chapter_136'
+# Temporary URL to parse.
+url = 'https://manganelo.com/manga/karakai_jouzu_no_takagisan'
 r = requests.get(url)
 
-html = r.content
-save_html(html, './manga.html')
+# Convert HTML raw text to soup.
+soup = BeautifulSoup(r.content, 'html.parser')
 
+# Get most recent chapter number.
+chapter_list = soup.select_one('.panel-story-chapter-list')
+recent_chapter = chapter_list.find('li', class_='a-h')
+chapter_title = recent_chapter.a['title']
+
+pattern = r'chapter\s*(\d*):*'
+match = re.search(pattern, chapter_title, re.IGNORECASE)
+
+if match:
+    chapter_num = match.group(1)
